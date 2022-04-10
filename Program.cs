8377be6bf79c6149bed25369f0a2c4fc92c8e3c2
Pawel Gajzler <pawel.gajzler.dev@gmail.com>
2022-04-10 08:30:49 +0200

    added temperature

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
+int GetTemperature(){
+    return 12;
+}
+
 Console.WriteLine("What's your name");
 string name = Console.ReadLine();
 DateTime now = DateTime.Now;
 string day = now.DayOfWeek;
 string time = now.ToString("HH:mm");
-Console.WriteLine($"Hello {name}. It's {day} and hour {time}");
+Console.WriteLine($"Hello {name}. It's {day} and hour {time}. Temperature outside is {GetTemperature()}");
