f5ed3ba52023612919504b55a67e843e373c666b
Pawel Gajzler <pawel.gajzler.dev@gmail.com>
2022-04-10 08:31:41 +0200

    refactoring

--- Program.cs
+++ Program.cs
@@ -7,4 +7,5 @@ string name = Console.ReadLine();
 DateTime now = DateTime.Now;
 string day = now.DayOfWeek;
 string time = now.ToString("HH:mm");
-Console.WriteLine($"Hello {name}. It's {day} and hour {time}. Temperature outside is {GetTemperature()}");
+string temperature = GetTemperature();
+Console.WriteLine($"Hello {name}. It's {day} and hour {time}. Temperature outside is {temperature}");
